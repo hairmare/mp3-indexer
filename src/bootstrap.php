596029fd1cebee7987a495e1d4994aae497748a6
Lucas S Bickel <hairmare@purplehaze.ch>
2012-03-05 22:55:33 +0100

    scans homedir and var_dumps id3 info or errors

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -11,7 +11,7 @@ sfServiceContainerAutoloader::register();
  
 // setup dependency injection
 $sc = new sfServiceContainerBuilder(array(
-    'mp3root' => $_SERVER['HOME'].'/Noiseshaper/'
+    'mp3root' => $_SERVER['HOME']
 ));
 
 $sc->register('dispatcher', 'sfEventDispatcher');
@@ -25,26 +25,22 @@ $sc->register('mp3iteratoriterator', 'RecursiveIteratorIterator')
    ->addArgument(new sfServiceReference('audiofilteriterator'))
    ->addArgument(RecursiveIteratorIterator::SELF_FIRST);
 
+// some events that folks may clone
 $sc->register('mp3fileevent', 'sfEvent')
    ->addArgument(new stdClass) // empty context because i dont care
    ->addArgument('mp3scan.file');
 $sc->register('mp3lintevent', 'sfEvent')
    ->addArgument(new stdClass) // empty context because i dont care
    ->addArgument('mp3scan.lint');
-
-
+// and my workhorses
 $sc->register('mp3scanner', 'Mp3Indexer_Scanner')
    ->addArgument(new sfServiceReference('mp3iteratoriterator'))
    ->addArgument(new sfServiceReference('dispatcher'))
    ->addArgument(new sfServiceReference('mp3fileevent'));
-
 $sc->register('mp3reader', 'Mp3Indexer_Reader')
    ->addArgument(new sfServiceReference('dispatcher'))
    ->addArgument(new sfServiceReference('mp3lintevent'));
-
+// as well as something to tie this together
 $sc->register('mp3indexer', 'Mp3Indexer')
    ->addArgument(new sfServiceReference('mp3scanner'))
    ->addArgument(new sfserviceReference('mp3reader'));
-
-// index.php
-$sc->mp3indexer->run();
