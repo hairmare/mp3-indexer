e9838e0ed83b660f63227bbabaf6df57699ceac2
Lucas S Bickel <hairmare@purplehaze.ch>
2012-05-19 14:06:04 +0200

    more build infra

---
+++ test/Bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * test bootstrap
+ *
+ * PHP Version 5
+ *
+ * @category  Application
+ * @package   Mp3Indexer
+ * @author    Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @copyright 2012 - Alle Rechte vorbehalten
+ * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link      http://github.com/purplehazech/mp3-indexer
+ */
+
+require_once dirname(__FILE__).'/../src/Bootstrap.php';
+
