492426a0ff614af0b46185932bd2414898d73acb
Lucas S Bickel <hairmare@purplehaze.ch>
2012-05-22 06:30:18 +0200

    comply with test

--- src/Mp3Indexer/Reader.php
+++ src/Mp3Indexer/Reader.php
@@ -59,8 +59,7 @@ class Mp3Indexer_Reader
     public function read(sfEvent $event)
     {
         $logError = false;
-        $file = $event['file'];
-        echo $file;
+        $file = $event->file;
 
         // lint them files
         $event = clone $this->_linter;
