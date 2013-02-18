066dadaa0869927e890adfeaed7c5405e410f4f7
Lucas Bickel <hairmare@purplehaze.ch>
2013-02-18 23:23:33 +0100

    call all methods in new test, no assertions yet

---
+++ test/Mp3Indexer/MwApiClientTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * runtime tests
+ *
+ * PHP Version 5
+ *
+ * @category  Test
+ * @package   Mp3Indexer
+ * @author    Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @copyright 2012 - Alle Rechte vorbehalten
+ * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link      http://github.com/purplehazech/mp3-indexer
+ */
+
+require_once __DIR__.'/../../src/Mp3Indexer/MwApiClient.php';
+
+/**
+ * Test class for Mp3Indexer_MwApiClient.
+ *
+ * @category Test
+ * @package  Mp3Indexer
+ * @author   Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @license  GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link     http://github.com/purplehazech/mp3-indexer
+ */
+class Mp3Indexer_MwApiClientTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Mp3Indexer_MwApiClient
+     */
+    protected $object;
+    
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->curlMock = $this
+            ->getMockBuilder('Mp3Indexer_Curl')
+            ->getMock();
+    
+        $this->object = new Mp3Indexer_MwApiClient(
+            'http://example.com/wiki/api.php',
+            $this->curlMock
+        );
+    }
+    
+    /**
+     * test login method
+     * 
+     * @return void
+     */
+    public function testLogin()
+    {
+        $this->object->login('testuser', 'testpass');
+    }
+    
+    /**
+     * simple incomplete test for sfautoedit
+     * 
+     * @return void
+     */
+    public function testSfautoedit()
+    {
+        $this->object->sfautoedit('TestFrom', 'TestTarget', array());
+    }
+}
