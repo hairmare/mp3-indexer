e5496d843a0ee14e1b6aa90b31f8e87182d6a89f
Lucas Bickel <hairmare@purplehaze.ch>
2013-02-20 20:51:19 +0100

    remove lint support from reader

--- src/Mp3Indexer/Reader.php
+++ src/Mp3Indexer/Reader.php
@@ -27,7 +27,6 @@ class Mp3Indexer_Reader
      * create reader and register event listeners
      *
      * @param sfEventDispatcher            $dispatcher    main event dispatcher
-     * @param sfEvent                      $linter        filter event for linting
      * @param sfEvent                      $dataEvent     event for storage dispatch
      * @param sfEvent                      $logEvent      logging event
      * @param Mp3Indexer_ReaderImplFactory $readerFactory factory for getting readers
@@ -36,14 +35,12 @@ class Mp3Indexer_Reader
      */
     public function __construct (
         sfEventDispatcher $dispatcher,
-        sfEvent $linter,
         sfEvent $dataEvent,
         sfEvent $logEvent,
         Mp3Indexer_ReaderImplFactory $readerFactory
     ) {
         $this->_dispatcher = $dispatcher;
         $this->_dispatcher->connect('mp3scan.file', array($this, 'read'));
-        $this->_linter = $linter;
         $this->_dataEvent = $dataEvent;
         $this->_logEvent = $logEvent;
         $this->_readerFactory = $readerFactory;
@@ -61,23 +58,16 @@ class Mp3Indexer_Reader
         $logError = false;
         $file = $event['file'];
 
-        // lint them files
-        $event = clone $this->_linter;
-        $this->_dispatcher->filter($event, $file);
-
         try {
             $reader = $this->_readerFactory->getReader($file);
-            if ($event->getReturnValue()) {
-                $data = $reader->getFramesByIdentifier('*');
+            
+            $data = $reader->getFramesByIdentifier('*');
 
-                $event = clone $this->_dataEvent;
-                $event['file'] = $file;
-                $event['data'] = $data;
-                $this->_dispatcher->notify($event);
-            } else {
-                $logError = true;
-                $logMessage = 'lintering skipped file';
-            }
+            $event = clone $this->_dataEvent;
+            $event['file'] = $file;
+            $event['data'] = $data;
+            $this->_dispatcher->notify($event);
+                
         } catch (Exception $e) {
             $logError = true;
             $logMessage = get_class($e).':'.$e->getMessage();
