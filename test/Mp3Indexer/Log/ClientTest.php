6c138805d598c51a3ff02db1ed45f3b97ea03404
Lucas Bickel <hairmare@purplehaze.ch>
2013-02-20 20:37:44 +0100

    bugfix #1

--- test/Mp3Indexer/Log/ClientTest.php
+++ test/Mp3Indexer/Log/ClientTest.php
@@ -23,7 +23,7 @@ require_once __DIR__.'/../../../src/Mp3Indexer/Log/Client.php';
  * @license  GPL http://www.gnu.org/licenses/gpl-3.0.txt
  * @link     http://github.com/purplehazech/mp3-indexer
  */
-class Mp3Indexer_Log_StdoutTest extends PHPUnit_Framework_TestCase
+class Mp3Indexer_Log_ClientTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @var Mp3Indexer_Log_Client
