e9aabd8f8aac39076449191d25a8dd8691516aad
Lucas Bickel <hairmare@purplehaze.ch>
2013-03-02 15:17:07 +0100

    work around smallish checkstyle bug

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -24,13 +24,12 @@ require_once dirname(__FILE__).
     '/../lib/sf-event-dispatcher/lib/sfEventDispatcher.php';
 
 // basic autoloader for Mp3Indexer_ Classes
-spl_autoload_register(
-    function ($class) {
-        if (substr($class, 0, 10) == 'Mp3Indexer') {
-            include __DIR__.'/'.strtr($class, '_', '/').'.php';
-        }
+$autoLoader = function ($class) {
+    if (substr($class, 0, 10) == 'Mp3Indexer') {
+        include __DIR__.'/'.strtr($class, '_', '/').'.php';
     }
-);
+};
+spl_autoload_register($autoLoader);
 sfServiceContainerAutoloader::register();
     
 // setup dependency injection
