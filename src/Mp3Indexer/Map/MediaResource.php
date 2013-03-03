403a574aea4ef95f62612c17c838d0753b1f5f06
Lucas Bickel <hairmare@purplehaze.ch>
2013-03-03 18:11:10 +0100

    checkstyle fixes

--- src/Mp3Indexer/Map/MediaResource.php
+++ src/Mp3Indexer/Map/MediaResource.php
@@ -21,20 +21,28 @@
  * @license  GPL http://www.gnu.org/licenses/gpl-3.0.txt
  * @link     http://github.com/purplehazech/mp3-indexer
  */
- class Mp3Indexer_Map_MediaResource extends Mp3Indexer_Map_SemanticMediawiki {
+class Mp3Indexer_Map_MediaResource extends Mp3Indexer_Map_SemanticMediawiki
+{
 
      const MW_FORM = 'MediaResource';
 
-     public function getElements()
-     {
-         $namespace = $this->getNamespace();
-         $base = '';
-         foreach (explode('/', dirname($this->getFile())) AS $dir) {
-             $base .= $dir.'/';
-             $elements[substr($namespace.$base, 0, -1)] = array(
-                 self::MW_FORM.'[Locator]=' => substr($base, 0, -1)
-             );
-         }
-         return $elements;
-     }
- }
+    /**
+     * return all mediaresource mapping for path segemments.
+     * 
+     * @see Mp3Indexer_Map_SemanticMediawiki::getElements()
+     * 
+     * @return Array
+     */
+    public function getElements()
+    {
+        $namespace = $this->getNamespace();
+        $base = '';
+        foreach (explode('/', dirname($this->getFile())) AS $dir) {
+            $base .= $dir.'/';
+            $elements[substr($namespace.$base, 0, -1)] = array(
+                self::MW_FORM.'[Locator]=' => substr($base, 0, -1)
+            );
+        }
+        return $elements;
+    }
+}
