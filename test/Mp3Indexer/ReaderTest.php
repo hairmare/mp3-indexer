3b7a869ab6b9551590a0f17f767df0a2cac2ca45
Lucas Bickel <hairmare@purplehaze.ch>
2013-03-03 18:08:29 +0100

    bump copyright year to 2013

--- test/Mp3Indexer/ReaderTest.php
+++ test/Mp3Indexer/ReaderTest.php
@@ -7,7 +7,7 @@
  * @category  Test
  * @package   Mp3Indexer
  * @author    Lucas S. Bickel <hairmare@purplehaze.ch>
- * @copyright 2012 - Alle Rechte vorbehalten
+ * @copyright 2012-2013 - Alle Rechte vorbehalten
  * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
  * @link      http://github.com/purplehazech/mp3-indexer
  */
