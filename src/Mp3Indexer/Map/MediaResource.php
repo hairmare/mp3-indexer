f702a62969e6d30f87f233fb22ccc1e4bd039e1e
Lucas Bickel <hairmare@purplehaze.ch>
2013-03-03 17:33:54 +0100

    test and stub for mediaresource map

---
+++ src/Mp3Indexer/Map/MediaResource.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * map data to semantic mediawiki MediaResource template
+ *
+ * PHP Version 5
+ *
+ * @category  Map
+ * @package   Mp3Indexer
+ * @author    Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @copyright 2013 - Alle Rechte vorbehalten
+ * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link      http://github.com/purplehazech/mp3-indexer
+ */
+
+/**
+ * map MediaResource data
+ *
+ * @category Map
+ * @package  Mp3Indexer
+ * @author   Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @license  GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link     http://github.com/purplehazech/mp3-indexer
+ */
+ class Mp3Indexer_Map_MediaResource extends Mp3Indexer_Map_SemanticMediawiki {
+
+     const MW_FORM = 'MediaResource';
+     
+     private $_templateMap = array(
+             self::ID3_ALBUM  => 'MediaResource[Locator]',
+     );
+     
+     public function getFile()
+     {
+         return parent::getFile();
+     }
+
+     public function getElements()
+     {
+         return array();
+     }
+     
+     public function getQuery()
+     {
+         return $this->_getQuery($this->_templateMap, self::MW_FORM);
+     }
+ }
---
+++ test/Mp3Indexer/Map/MediaResourceTest.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * runtime tests
+ *
+ * PHP Version 5
+ *
+ * @category  Test
+ * @package   Mp3Indexer
+ * @author    Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @copyright 2012 - Alle Rechte vorbehalten
+ * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link      http://github.com/purplehazech/mp3-indexer
+ */
+
+require_once __DIR__.'/../../../src/Mp3Indexer/Map/SemanticMediawiki.php';
+require_once __DIR__.'/../../../src/Mp3Indexer/Map/MediaResource.php';
+
+/**
+ * Test class for Mp3Indexer_Map_AudioTrack.
+ *
+ * @category Test
+ * @package  Mp3Indexer
+ * @author   Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @license  GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link     http://github.com/purplehazech/mp3-indexer
+ */
+class Mp3Indexer_Map_MediaResourceTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Mp3Indexer_Map_AudioTrack
+     */
+    protected $object;
+    
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->textFrameMock = $this
+            ->getMockBuilder('Zend_Media_Id3_TextFrame')
+            ->setMethods(
+                array(
+                    'getIdentifier',
+                    'getTexts'
+                )
+            )
+            ->getMock();
+        
+        $this->object = new Mp3Indexer_Map_MediaResource();
+    }
+    
+    /**
+     * test getTarget Method
+     *
+     * @return void
+     */
+    public function testGetTarget()
+    {
+        $data = array(
+            'file' => 'testdir/testfile'
+        );
+        $this->object->setData($data);
+        
+        $this->assertEquals(
+            $this->object->getTarget(),
+            'testdir/testfile'
+        );
+    }
+
+    /**
+     * test getElements Method
+     *
+     * @return void
+     */
+    public function testGetElements()
+    {
+        $data = array(
+            'file' => 'testdir/testfile'
+        );
+        $this->object->setData($data);
+        
+        $this->textFrameMock
+            ->expects($this->atLeastOnce())
+            ->method('getIdentifier')
+            ->will($this->returnValue('TALB'));
+        $this->textFrameMock
+            ->expects($this->atLeastOnce())
+            ->method('getTexts')
+            ->will($this->returnValue(array('Hello World!')));
+        
+        $data[] = $this->textFrameMock;
+        $data[] = $this->textFrameMock;
+        $this->object->setData($data);
+    
+        $this->assertEquals(
+            $this->object->getElements(),
+            array(
+                'testdir' => array(
+                    'MediaResource[Locator]=' => 'testdir/',
+                )    
+            )
+        );
+    }
+}
